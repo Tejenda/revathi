fc1341dedbc5a3ea1f2017ad8165c794448da9f0
Tejendra <tejendraswayampu1825@gmail.com>
2025-05-09 14:05:44 +0530

    added to githuh python programs.py

---
+++ python programs.py/dictionary.py
@@ -0,0 +1,13 @@
+'''person = {"name": "Alice", "age": 30}
+print(person["name"])   # Output: Alice
+person["age"] = 31
+print(person)           # Output: {'name': 'Alice', 'age': 31}'''
+
+
+
+
+
+person ={"name": "teja", "age":"24", "DOB":"18-10-2001"}
+print(person["name"])
+print(person["age"])
+print(person)
